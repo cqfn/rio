6d25cedf446fa2d0e605e32caa20443143854a99
Kirill <g4s8.public@gmail.com>
2020-07-23 18:53:47 +0300

    #12 - prepare for cqfn

---
+++ src/main/java/org/cqfn/rio/file/File.java
@@ -0,0 +1,184 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 cqfn.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights * to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.cqfn.rio.file;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.reactivestreams.Publisher;
+
+/**
+ * Reactive file API.
+ * @since 0.1
+ */
+public final class File {
+
+    /**
+     * Default executor.
+     */
+    private static final ExecutorService EXEC_DEFAULT = Executors.newCachedThreadPool();
+
+    /**
+     * File path.
+     */
+    private final Path path;
+
+    /**
+     * New file.
+     * @param path Path
+     */
+    public File(final Path path) {
+        this.path = path;
+    }
+
+    /**
+     * File's content.
+     * @return Content publisher
+     */
+    public Publisher<ByteBuffer> content() {
+        return this.content(Buffers.Standard.K8);
+    }
+
+    /**
+     * File's content.
+     * @param buf Buffers policy
+     * @return Content publisher
+     */
+    public Publisher<ByteBuffer> content(final Buffers buf) {
+        return this.content(buf, File.EXEC_DEFAULT);
+    }
+
+    /**
+     * File's content.
+     * @param exec Executor service to perform IO operations
+     * @return Content publisher
+     */
+    public Publisher<ByteBuffer> content(final ExecutorService exec) {
+        return new ReadFlow(this.path, Buffers.Standard.K8, exec);
+    }
+
+    /**
+     * File's content.
+     * @param buf Buffers policy
+     * @param exec Executor service to perform IO operations
+     * @return Content publisher
+     */
+    public Publisher<ByteBuffer> content(final Buffers buf, final ExecutorService exec) {
+        return new ReadFlow(this.path, buf, exec);
+    }
+
+    /**
+     * Write data to file.
+     * @param data Data publisher
+     * @param opts Options
+     * @return Future
+     */
+    public CompletionStage<Void> write(final Publisher<ByteBuffer> data, final OpenOption... opts) {
+        return this.write(data, WriteGreed.SYSTEM, opts);
+    }
+
+    /**
+     * Write data to file.
+     * @param data Data publisher
+     * @param exec Executor service to perform IO operations
+     * @param opts Options
+     * @return Future
+     */
+    public CompletionStage<Void> write(final Publisher<ByteBuffer> data,
+        final ExecutorService exec,
+        final OpenOption... opts) {
+        return this.write(data, WriteGreed.SYSTEM, exec, opts);
+    }
+
+    /**
+     * Write data to file.
+     * @param data Data publisher
+     * @param greed Greed level of consumer
+     * @param opts Options
+     * @return Future
+     */
+    public CompletionStage<Void> write(final Publisher<ByteBuffer> data, final WriteGreed greed,
+        final OpenOption... opts) {
+        return this.write(data, greed, File.EXEC_DEFAULT, opts);
+    }
+
+    /**
+     * Write data to file.
+     * @param data Data publisher
+     * @param greed Greed level of consumer
+     * @param exec Executor service to perform IO operations
+     * @param opts Options
+     * @return Future
+     * @checkstyle ReturnCountCheck (20 lines)
+     * @checkstyle ParameterNumberCheck (7 lines)
+     */
+    @SuppressWarnings("PMD.OnlyOneReturn")
+    public CompletionStage<Void> write(final Publisher<ByteBuffer> data,
+        final WriteGreed greed,
+        final ExecutorService exec,
+        final OpenOption... opts) {
+        final WriteSubscriber sub;
+        try {
+            sub = new WriteSubscriber(
+                FileChannel.open(
+                    this.path,
+                    new HashSet<>(Arrays.asList(writeOpts(opts)))
+                ),
+                greed,
+                exec
+            );
+        } catch (final IOException err) {
+            final CompletableFuture<Void> res = new CompletableFuture<>();
+            res.completeExceptionally(err);
+            return res;
+        }
+        data.subscribe(sub);
+        return sub;
+    }
+
+    /**
+     * Write options.
+     * @param src User specified options
+     * @return Fixed options
+     */
+    private static OpenOption[] writeOpts(final OpenOption... src) {
+        final OpenOption[] opts;
+        if (src.length == 0) {
+            opts = new OpenOption[]{StandardOpenOption.WRITE, StandardOpenOption.CREATE};
+        } else {
+            opts = src;
+        }
+        return opts;
+    }
+}
---
+++ src/main/java/org/cqfn/rio/file/ReadRequest.java
@@ -0,0 +1,143 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 cqfn.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights * to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.cqfn.rio.file;
+
+import com.jcabi.log.Logger;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Read request.
+ * @since 0.1
+ * @checkstyle ExecutableStatementCountCheck (500 lines)
+ */
+@SuppressWarnings("PMD.MissingStaticMethodInNonInstantiatableClass")
+abstract class ReadRequest {
+
+    /**
+     * Flow subscriber.
+     * @checkstyle VisibilityModifierCheck (5 lines)
+     */
+    protected final ReadSubscriberState<? super ByteBuffer> sub;
+
+    /**
+     * New read request.
+     * @param sub Subscriber
+     */
+    private ReadRequest(final ReadSubscriberState<? super ByteBuffer> sub) {
+        this.sub = sub;
+    }
+
+    /**
+     * Process file channel.
+     * @param channel Channel to process
+     */
+    abstract void process(FileChannel channel);
+
+    /**
+     * Next request.
+     * @since 0.1
+     */
+    static final class Next extends ReadRequest {
+
+        /**
+         * Buffers.
+         */
+        private final Buffers buffers;
+
+        /**
+         * Count to request.
+         */
+        private final long count;
+
+        /**
+         * New read request.
+         * @param sub Subscriber
+         * @param buffers Buffer allocation strategy
+         * @param count Amount of requests
+         */
+        Next(final ReadSubscriberState<? super ByteBuffer> sub, final Buffers buffers,
+            final long count) {
+            super(sub);
+            this.buffers = buffers;
+            this.count = count;
+        }
+
+        // @checkstyle ReturnCountCheck (50 lines)
+        @Override
+        @SuppressWarnings({"PMD.OnlyOneReturn", "PMD.AvoidCatchingGenericException"})
+        void process(final FileChannel channel) {
+            for (int cnt = 0; cnt < this.count; ++cnt) {
+                if (this.sub.done()) {
+                    return;
+                }
+                final ByteBuffer buf = this.buffers.create();
+                final int read;
+                try {
+                    read = channel.read(buf);
+                } catch (final IOException iex) {
+                    try {
+                        channel.close();
+                    } catch (final IOException cex) {
+                        Logger.warn(
+                            this,
+                            "Failed to close channel on errors: %[exception]s", cex
+                        );
+                    }
+                    this.sub.onError(iex);
+                    return;
+                }
+                ((Buffer) buf).flip();
+                if (read >= 0) {
+                    try {
+                        this.sub.onNext(buf);
+                        // @checkstyle IllegalCatchCheck (1 line)
+                    } catch (final Exception exx) {
+                        try {
+                            channel.close();
+                        } catch (final IOException cex) {
+                            Logger.warn(
+                                this,
+                                "Failed to close channel on next error: %[exception]s", cex
+                            );
+                        }
+                        this.sub.onError(exx);
+                        return;
+                    }
+                } else {
+                    try {
+                        channel.close();
+                        this.sub.onComplete();
+                    } catch (final IOException iex) {
+                        this.sub.onError(iex);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
