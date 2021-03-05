f2ee2487d7a8c6af171ddfd9814f328038eb1017
Kirill <g4s8.public@gmail.com>
2021-03-05 20:31:42 +0300

    For #45 - replaced asto with pure vertx

--- benchmarks/src/main/java/org/cqfn/rio/bench/VertxTarget.java
+++ benchmarks/src/main/java/org/cqfn/rio/bench/VertxTarget.java
@@ -1,6 +1,5 @@
 package org.cqfn.rio.bench;
 
-import com.artipie.asto.fs.VertxRxFile;
 import hu.akarnokd.rxjava2.interop.CompletableInterop;
 import io.reactivex.Flowable;
 import io.vertx.reactivex.core.Vertx;
@@ -8,6 +7,15 @@ import java.nio.ByteBuffer;
 import java.nio.file.Path;
 import java.util.concurrent.CompletableFuture;
 import org.reactivestreams.Publisher;
+import io.vertx.core.file.CopyOptions;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.reactivex.RxHelper;
+import io.vertx.reactivex.core.Promise;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.buffer.Buffer;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
 
 /**
  * Vertx benchmark test provider.
@@ -18,14 +26,70 @@ public final class VertxTarget implements BenchmarkTarget {
 
     @Override
     public Publisher<ByteBuffer> read(final Path path) {
-        return new VertxRxFile(path, this.vertx).flow();
+        return this.vertx.fileSystem().rxOpen(
+            path.toString(),
+            new OpenOptions()
+                .setRead(true)
+                .setWrite(false)
+                .setCreate(false)
+        )
+            .flatMapPublisher(
+                asyncFile -> {
+                    final Promise<Void> promise = Promise.promise();
+                    final Completable completable = Completable.create(
+                        emitter ->
+                            promise.future().onComplete(
+                                event -> {
+                                    if (event.succeeded()) {
+                                        emitter.onComplete();
+                                    } else {
+                                        emitter.onError(event.cause());
+                                    }
+                                }
+                            )
+                    );
+                    return asyncFile.toFlowable().map(
+                        buffer -> ByteBuffer.wrap(buffer.getBytes())
+                    ).doOnTerminate(() -> asyncFile.rxClose().subscribe(promise::complete))
+                        .mergeWith(completable);
+                }
+            );
     }
 
     @Override
     public CompletableFuture<?> write(final Path path, final Publisher<ByteBuffer> data) {
-        return new VertxRxFile(path, this.vertx)
-            .save(Flowable.fromPublisher(data))
-            .to(CompletableInterop.await())
-            .toCompletableFuture();
+        return this.vertx.fileSystem().rxOpen(
+            path.toString(),
+            new OpenOptions()
+                .setRead(false)
+                .setCreate(true)
+                .setWrite(true)
+                .setTruncateExisting(true)
+        )
+            .flatMapCompletable(
+                asyncFile -> Completable.create(
+                    emitter -> {
+                        Flowable.fromPublisher(data).map(buf -> Buffer.buffer(bts(buf)))
+                            .onErrorResumeNext(
+                                thr -> {
+                                    return asyncFile.rxClose().andThen(Flowable.error(thr));
+                                }
+                            )
+                            .subscribe(asyncFile.toSubscriber()
+                                .onWriteStreamEnd(emitter::onComplete)
+                                .onWriteStreamError(emitter::onError)
+                                .onWriteStreamEndError(emitter::onError)
+                                .onError(emitter::onError)
+                            );
+                    }
+                )
+            ).to(CompletableInterop.await()).toCompletableFuture();
     }
+
+    private static byte[] bts(ByteBuffer bb) {
+        final byte[] arr = new byte[bb.remaining()];
+        bb.get(arr);
+        return arr;
+    }
+
 }
