f92ff8698fd40ad4ac07e3272e5797dd9e878316
olenagerasimova <olena.gerasimova@gmail.com>
2021-03-26 15:43:28 +0300

    Added more tests for ReactiveOutputStreamTest

--- src/test/java/org/cqfn/rio/stream/ReactiveOutputStreamTest.java
+++ src/test/java/org/cqfn/rio/stream/ReactiveOutputStreamTest.java
@@ -27,9 +27,12 @@ package org.cqfn.rio.stream;
 import io.reactivex.Flowable;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
+import java.util.Objects;
 import org.cqfn.rio.WriteGreed;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
@@ -38,6 +41,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Test for {@link ReactiveOutputStream}.
  * @since 0.4
+ * @checkstyle MagicNumberCheck (500 lines)
  */
 class ReactiveOutputStreamTest {
 
@@ -71,6 +75,57 @@ class ReactiveOutputStreamTest {
                 new IsEqual<>(data)
             );
         }
+    }
+
+    @Test
+    void transferResourceToStream() {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final byte[] data = this.getBinFile();
+        new ReactiveOutputStream(out)
+            .write(Flowable.fromArray(ByteBuffer.wrap(data)), WriteGreed.SYSTEM)
+            .toCompletableFuture().join();
+        MatcherAssert.assertThat(
+            out.toByteArray(),
+            new IsEqual<>(data)
+        );
+    }
+
+    @Test
+    void transferResourceToPipedStream() throws IOException {
+        try (
+            PipedInputStream in = new PipedInputStream();
+            PipedOutputStream out = new PipedOutputStream(in)
+        ) {
+            final byte[] data = this.getBinFile();
+            final byte[] res = new byte[data.length];
+            new ReactiveOutputStream(out)
+                .write(Flowable.fromArray(ByteBuffer.wrap(data)), WriteGreed.SYSTEM)
+                .toCompletableFuture().join();
+            in.read(res);
+            MatcherAssert.assertThat(
+                res,
+                new IsEqual<>(data)
+            );
+        }
+    }
+
+    @SuppressWarnings("PMD.AssignmentInOperand")
+    private byte[] getBinFile() {
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try (
+            InputStream stream = Objects.requireNonNull(
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("file.bin")
+            )
+        ) {
+            int count;
+            final byte[] data = new byte[8 * 1024];
+            while ((count = stream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, count);
+            }
+        } catch (final IOException err) {
+            throw new UncheckedIOException(err);
+        }
+        return buffer.toByteArray();
     }
 
 }
