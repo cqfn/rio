c697f1a2ca774b03a0c2e23ea8037c683c9729a8
Alena <olena.gerasimova@gmail.com>
2021-03-30 09:14:52 +0300

    More tests for ReactiveOutputStream
    Added more tests against ReactiveOutputStream:
     - Added test for ReactiveOutputStreamTest
     - Added more tests for ReactiveOutputStreamTest
    
    PR: #50

--- src/test/java/org/cqfn/rio/stream/ReactiveOutputStreamTest.java
+++ src/test/java/org/cqfn/rio/stream/ReactiveOutputStreamTest.java
@@ -27,9 +27,13 @@ package org.cqfn.rio.stream;
 import io.reactivex.Flowable;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import org.cqfn.rio.WriteGreed;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
@@ -38,6 +42,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Test for {@link ReactiveOutputStream}.
  * @since 0.4
+ * @checkstyle MagicNumberCheck (500 lines)
  */
 class ReactiveOutputStreamTest {
 
@@ -55,17 +60,23 @@ class ReactiveOutputStreamTest {
     }
 
     @Test
+    @SuppressWarnings("PMD.AssignmentInOperand")
     void transferDataToPipedStream() throws IOException {
         try (
             PipedInputStream in = new PipedInputStream();
             PipedOutputStream out = new PipedOutputStream(in)
         ) {
-            final byte[] data = "abc".getBytes();
+            final byte[] data = this.getBinFile();
             final byte[] res = new byte[data.length];
-            new ReactiveOutputStream(out)
+            final CompletableFuture<Void> future = new ReactiveOutputStream(out)
                 .write(Flowable.fromArray(ByteBuffer.wrap(data)), WriteGreed.SYSTEM)
-                .toCompletableFuture().join();
-            in.read(res);
+                .toCompletableFuture();
+            int offset = 0;
+            int read;
+            while ((read = in.read(res, offset, res.length - offset)) > 0) {
+                offset += read;
+            }
+            future.join();
             MatcherAssert.assertThat(
                 res,
                 new IsEqual<>(data)
@@ -73,4 +84,35 @@ class ReactiveOutputStreamTest {
         }
     }
 
+    @Test
+    void transferResourceToStream() {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final byte[] data = this.getBinFile();
+        new ReactiveOutputStream(out)
+            .write(Flowable.fromArray(ByteBuffer.wrap(data)), WriteGreed.SYSTEM)
+            .toCompletableFuture().join();
+        MatcherAssert.assertThat(
+            out.toByteArray(),
+            new IsEqual<>(data)
+        );
+    }
+
+    @SuppressWarnings("PMD.AssignmentInOperand")
+    private byte[] getBinFile() {
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try (
+            InputStream stream = Objects.requireNonNull(
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("file.bin")
+            )
+        ) {
+            int count;
+            final byte[] data = new byte[8 * 1024];
+            while ((count = stream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, count);
+            }
+        } catch (final IOException err) {
+            throw new UncheckedIOException(err);
+        }
+        return buffer.toByteArray();
+    }
 }
